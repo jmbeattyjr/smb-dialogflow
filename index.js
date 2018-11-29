976034480cb3ba05a6858a653f320bb7f7bfca55
mbeatty <jmbjr@1791.com>
2018-11-29 13:06:15 -0500

    and now again

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ app.fallback('Default Fallback Intent', (conv) => {
     conv.ask('Please repeat');
 });
 
-app.intent('Default Welcome Intent', (conv) => {
-    conv.ask('Hi, what do you wanna talk about?');
-    conv.ask(new Suggestions(['fashion tips', 'celebrity news']));
-});
+// app.intent('Default Welcome Intent', (conv) => {
+//     conv.ask('Hi, what do you wanna talk about?');
+//     conv.ask(new Suggestions(['fashion tips', 'celebrity news']));
+// });
 
 // app.fallback((conv) => {
 //     // intent contains the name of the intent
