161556999815515486b99984747989535fe1fe1b
mbeatty <jmbjr@1791.com>
2018-11-29 13:12:46 -0500

    more testing

--- index.js
+++ index.js
@@ -8,29 +8,29 @@ const functions = require('firebase-functions');
 
 const app = dialogflow({debug: true});
 
-app.intent('Default Fallback Intent', (conv) => {
-    conv.ask('Please repeat');
-});
+// app.intent('Default Fallback Intent', (conv) => {
+//     conv.ask('Please repeat');
+// });
 
-app.intent('Default Welcome Intent', (conv) => {
-    conv.ask('Hi, what do you wanna talk about?');
-    conv.ask(new Suggestions(['fashion tips', 'celebrity news']));
-});
+// app.intent('Default Welcome Intent', (conv) => {
+//     conv.ask('Hi, what do you wanna talk about?');
+//     conv.ask(new Suggestions(['fashion tips', 'celebrity news']));
+// });
 
-// app.fallback((conv) => {
-//     // intent contains the name of the intent
-//     // you defined in the Intents area of Dialogflow
-//     const intent = conv.intent;
-//     switch (intent) {
-//       case WELCOME_INTENT:
-//         conv.ask('Welcome! Say a number.');
-//         break;
-//       case NUMBER_INTENT:
-//         const num = conv.arguments.get(NUMBER_ARGUMENT);
-//         conv.close(`You said ${num}`);
-//         break;
-//     }
-//    });
+app.fallback((conv) => {
+    // intent contains the name of the intent
+    // you defined in the Intents area of Dialogflow
+    const intent = conv.intent;
+    switch (intent) {
+      case WELCOME_INTENT:
+        conv.ask('Welcome! Say a number.');
+        break;
+      case NUMBER_INTENT:
+        const num = conv.arguments.get(NUMBER_ARGUMENT);
+        conv.close(`You said ${num}`);
+        break;
+    }
+   });
 
 // handlers for other intents...
 
