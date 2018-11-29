d5f78e06d49e3eec94197819da4e150c466c5690
mbeatty <jmbjr@1791.com>
2018-11-29 13:00:24 -0500

    maybe now

--- index.js
+++ index.js
@@ -17,6 +17,21 @@ app.intent('Default Welcome Intent', (conv) => {
     conv.ask(new Suggestions(['fashion tips', 'celebrity news']));
 });
 
+app.fallback((conv) => {
+    // intent contains the name of the intent
+    // you defined in the Intents area of Dialogflow
+    const intent = conv.intent;
+    switch (intent) {
+      case WELCOME_INTENT:
+        conv.ask('Welcome! Say a number.');
+        break;
+      case NUMBER_INTENT:
+        const num = conv.arguments.get(NUMBER_ARGUMENT);
+        conv.close(`You said ${num}`);
+        break;
+    }
+   });
+
 // handlers for other intents...
 
 const express = require('express');
